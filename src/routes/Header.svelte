16613de41b4194f1d3ea7a32abaf79e18c9fd96c
externref <shiva02939@gmail.com>
2024-02-21 02:27:15 +0530

    fix flowbite

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,12 +1,8 @@
-<nav
-	class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
->
+<nav class="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
 			<img src="/lemon.png" class="h-8" alt="Lemon Logo" />
-			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-				>externref</span
-			>
+			<span class="self-center text-2xl font-semibold whitespace-nowraptext-white">externref</span>
 		</a>
 		<div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 			<button
@@ -47,26 +43,26 @@
 			id="navbar-sticky"
 		>
 			<ul
-				class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+				class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700"
 			>
 				<li>
 					<a
 						href="/"
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white border-gray-700"
 						aria-current="page">Home</a
 					>
 				</li>
 				<li>
 					<a
 						href="/#projects"
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white border-gray-700"
 						>Projects</a
 					>
 				</li>
 				<li>
 					<a
 						href="/blog"
-						class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+						class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 md:hover:text-blue-500 text-white hover:bg-gray-700 hover:text-white border-gray-700"
 						>Blog</a
 					>
 				</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [require('flowbite/plugin')]
 };
