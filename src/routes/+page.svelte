6fc5c0cbb649d81110ee71d2cd46202c567e8fcb
externref <shiva02939@gmail.com>
2024-11-18 20:29:14 +0530

    update sum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+	import Connect from './Connect.svelte';
+
+	let hello = '';
+	let sarthak = '';
+	let showCursor = true;
+
+	function typewriter() {
+		if (hello.length !== "Hello, I'm".length) {
+			hello += "Hello, I'm".at(hello.length);
+			setTimeout(typewriter, 50);
+		} else if (sarthak.length !== 'Sarthak'.length) {
+			sarthak += 'Sarthak'.at(sarthak.length);
+			setTimeout(typewriter, 50);
+		} else {
+			toggleCursor();
+		}
+	}
+
+	function toggleCursor() {
+		showCursor = !showCursor;
+		setTimeout(toggleCursor, 500);
+	}
+
+	onMount(() => {
+		typewriter();
+	});
+</script>
 
 <svelte:head>
-	<title>externref | Home</title>
+	<title>/home/externref</title>
 	<meta content="externref.tech" property="og:title" />
 	<meta content="personal portfolio and blog." property="og:description" />
 	<meta content="/" property="og:url" />
@@ -9,3 +38,63 @@
 	<meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
+<div id="info" class="md:flex font-exo2">
+	<div class="">
+		<div class="flex text-4xl text-center">
+			<div class="">{hello}</div>
+			<div class="text-[#4d557d]">&nbsp;{sarthak}</div>
+			<div class={`ml-2 leading-none ${showCursor ? 'animate-blink' : ''}`}>|</div>
+		</div>
+		<div class="mt-3 ml-1 text-base">
+			Programming nerd deeply invested into learning backends, CLIs, APIs, Desktop applications and
+			working of Operating systems. I'm interested in low level languages and the UNIX development
+			environments, however, open to all new technologies. I'm driven by a love for problem-solving
+			and crafting elegant, maintainable code. Whether working on personal projects or collaborating
+			with others, I approach each challenge with enthusiasm and a relentless pursuit of excellence,
+			always seeking to expand my skills and contribute to innovative software solutions.
+			<i class="text-[#1A1A1D] hover:text-[#4d557d]">I use Arch btw</i>
+		</div>
+		<div class="mt-4 text-sm md:text-lg">
+			<a href="#projects"
+				><button class="bg-[#4d557d] m-1 px-2 py-1 rounded-sm">
+					<i class="bi bi-braces"></i> Projects</button
+				></a
+			>
+			<a href="#contact">
+				<button class="bg-[#4d557d] m-1 px-2 py-1 rounded-sm"
+					><i class="bi bi-person-raised-hand"></i> Contact</button
+				></a
+			><a href="https://yap.externref.tech"
+				><button class="bg-[#4d557d] m-1 px-2 py-1 rounded-sm"
+					><i class="bi bi-megaphone-fill"></i> Blogs</button
+				></a
+			><a href="https://github.com/externref"
+				><button class="bg-[#4d557d] m-1 px-2 py-1 rounded-sm"
+					><i class="bi bi-github"></i> Github</button
+				>
+			</a>
+		</div>
+	</div>
+
+	<img src="/site_banner.png" alt="site banner" class="md:w-[400px]" />
+</div>
+
+<Connect />
+
+<style>
+	@keyframes blink {
+		0% {
+			opacity: 1;
+		}
+		50% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.animate-blink {
+		animation: blink 0.5s infinite;
+	}
+</style>
---
+++ src/routes/Connect.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	const urls = {
+		github: {
+			name: 'Github',
+			url: 'https://github.com/externref'
+		},
+		steam: {
+			name: 'Steam',
+			url: 'https://steamcommunity.com/id/externref/'
+		},
+		linkedin: {
+			name: 'LinkedIn',
+			url: 'https://linkedin.com/in/defsarthak'
+		},
+		discord: {
+			name: 'Discord',
+			url: 'https://discord.com/users/1134016724132446208'
+		},
+		'envelope-fill': {
+			name: 'E-Mail',
+			url: 'mailto:shiva02939@gmail.com'
+		},
+		instagram: {
+			name: 'Instagram',
+			url: 'https://instagram.com/sarthak.sh_'
+		}
+	};
+</script>
+
+<div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-8">
+	<div>
+		<div class="text-6xl md:text-7xl font-exo2">
+			<span id="connect" class="text-[#4d557d]">Connect </span> with me.
+		</div>
+		<div class="mt-4">
+			Feel free to connect with me on these platforms for any kind of discussions or collaborations!
+		</div>
+	</div>
+
+	<div class="md:mt-5 flex font-exo2 items-center justify-center">
+		<div class="grid md:grid-flow-row justify-between grid-cols-3 font-exo2">
+			{#each Object.entries(urls) as [name, url]}
+				<div class="p-1">
+					<button class="bg-[#4d557d] py-1 px-2 rounded-sm"
+						><a
+							href={url.url}
+							target="_blank"
+							class="flex items-center space-x-2 text-sm md:text-lg"
+						>
+							<i class="bi bi-{name}"></i>
+							<span>{url.name}</span>
+						</a></button
+					>
+				</div>
+			{/each}<br />
+			<div class="bg-green-50 mt-5 h-1">&nbsp;</div>
+		</div>
+	</div>
+</div>
